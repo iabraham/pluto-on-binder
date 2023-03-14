0d51eabb4275f991eac4a9f0dbc17917c00dd39d
Ivan Abraham <ivan.abrms@gmail.com>
2023-03-13 20:32:18 -0500

    Testing precompiling development packages on Binder

--- setup.py
+++ setup.py
@@ -15,4 +15,4 @@ setuptools.setup(
 
 # because this is a demo of Pluto, we add some popular packages to the global package env and precompile
 import os
-os.system('julia -e "import Pkg; Pkg.add([\\"DataFrames\\", \\"CSV\\", \\"Plots\\"]); Pkg.precompile()"')
+os.system('julia -e "import Pkg; Pkg.add([\\"DataFrames\\", \\"CSV\\", \\"Plots\\"]); Pkg.add(url=\\"https://gitlab.engr.illinois.edu/itabrah2/Ellipses.jl.git\\"); Pkg.add(url=\\"https://gitlab.engr.illinois.edu/itabrah2/Cyclicity.jl.git\\"); Pkg.precompile()"')
