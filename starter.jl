2d5d838f5fcc7dfadea851bfa2ce5f161038dad6
Ivan Abraham <ivan.abrms@gmail.com>
2023-03-18 11:52:59 -0500

    Test a starter notebook file

---
+++ starter.jl
@@ -0,0 +1,15 @@
+### A Pluto.jl notebook ###
+# v0.19.22
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ ae4fb092-b3d4-4035-89fb-257125d73252
+begin 
+	using Pkg
+	Pkg.add(url="https://gitlab.engr.illinois.edu/itabrah2/Ellipses.jl")
+	Pkg.add(url="https://gitlab.engr.illinois.edu/itabrah2/Cyclicity.jl")
+end
+
+# ╔═╡ Cell order:
+# ╠═ae4fb092-b3d4-4035-89fb-257125d73252
